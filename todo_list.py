f53124128f50966071df3954581a79e9e7df6762
liheng1818 <146962672+liheng1818@users.noreply.github.com>
2023-10-04 21:03:16 +0700

    Create todo_list.py

---
+++ todo_list.py
@@ -0,0 +1,15 @@
+''' 
+document:
+1. add task: ToDoList.add_task(id, title, created_date, description, tags)
+    parameter: id, title, created_date must be given
+2. get all tasks: ToDoList.get_all_tasks()
+3. get a task: ToDolist.get_task(id)
+    parameter: id
+4. remove task: ToDolist.remove_task(id)
+    parameter: id
+5. filter tasks by date: get_task_by_date(start_date, end_date = None)
+    parameter:
+        start_date: start_date to search tasks
+        end_date: end_date to search tasks, if None, only equal search for start_date
+6. 
+'''
